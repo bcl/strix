a004c7d292f0ca931c7d6914a4f0ff5c2bf55d12
Brian C. Lane <bcl@brianlane.com>
2022-03-20 09:17:45 -0700

    Start using modern build system configuration
    Things have changed since I originally wrote this. It's time to move the
    configuration into setup.cfg and the new pyproject.toml files. Simplify
    the requirements so that they are only listed in setup.cfg, not in 3
    different files.
    
    NOTE: While tox.ini can support embedding the ini in pyproject.toml it
    is only as a raw string so there is no point in switching that at this
    time.

--- setup.py
+++ setup.py
@@ -1,40 +1,4 @@
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import sys
+# See setup.cfg
+from setuptools import setup
 
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = ""
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-
-        # It doesn't use the ini by default for some reason
-        self.tox_args += " -c tox.ini"
-        errno = tox.cmdline(args=shlex.split(self.tox_args))
-        sys.exit(errno)
-
-setup(
-    name="strix",
-    version="0.0.1",
-    packages=find_packages(),
-    package_dir = {"strix": "src/strix"},
-    package_data = {"strix": ["ui/*"]},
-    setup_requires=['nose>=1.0', 'setuptools-lint'],
-    tests_require=['tox', 'coverage', 'nose', 'pylint'],
-    cmdclass={'test': Tox},
-
-    author="Brian Lane",
-    author_email="bcl@brianlane.com",
-    description="A Motion Camera API Server and UI",
-    license="GPLv2",
-    keywords="motion security camera strix",
-    url="https://www.brianlane.com/software/strix.html"
-)
+setup()
